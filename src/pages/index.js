1156243fb869aec8e265118a172b44c882f53786
Gleb Y Panteleev <dev@gleb.pw>
2021-08-03 10:07:09 +0200

    hi video

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,22 +4,12 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    {/*<Seo title="Home" />*/}
-    <h1>Hi people</h1>
-    {/*<p>Welcome to your new Gatsby site.</p>*/}
-    {/*<p>Now go build something great.</p>*/}
-    {/*<StaticImage*/}
-    {/*  src="../images/gatsby-astronaut.png"*/}
-    {/*  width={300}*/}
-    {/*  quality={95}*/}
-    {/*  formats={["AUTO", "WEBP", "AVIF"]}*/}
-    {/*  alt="A Gatsby astronaut"*/}
-    {/*  style={{ marginBottom: `1.45rem` }}*/}
-    {/*/>*/}
-    {/*<p>*/}
-    {/*  <Link to="/page-2/">Go to page 2</Link> <br />*/}
-    {/*  <Link to="/using-typescript/">Go to "Using TypeScript"</Link>*/}
-    {/*</p>*/}
+    <div style="position: relative; padding-top: 56.25%;">
+      <iframe src="https://iframe.mediadelivery.net/embed/10099/b4feb7f9-b80b-4e81-8273-bc74593f43e2?autoplay=false"
+              loading="lazy" style="border: none; position: absolute; top: 0; height: 100%; width: 100%;"
+              allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+              allowFullScreen="true"></iframe>
+    </div>
   </Layout>
 )
 
