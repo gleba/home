8a69a4ccfa6edbb0f0428dc64259d4a571756776
Gleb Y Panteleev <dev@gleb.pw>
2021-08-03 09:53:06 +0200

    hi people

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,9 +7,7 @@
 
 import * as React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import { graphql, useStaticQuery } from "gatsby"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,25 +23,26 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      {children}
+      {/*<Header siteTitle={data.site.siteMetadata?.title || `Title`} />*/}
+      {/*<div*/}
+      {/*  style={{*/}
+      {/*    margin: `0 auto`,*/}
+      {/*    maxWidth: 960,*/}
+      {/*    padding: `0 1.0875rem 1.45rem`,*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  <main>{children}</main>*/}
+      {/*  <footer*/}
+      {/*    style={{*/}
+      {/*      marginTop: `2rem`,*/}
+      {/*    }}*/}
+      {/*  >*/}
+      {/*    © {new Date().getFullYear()}, Built with*/}
+      {/*    {` `}*/}
+      {/*    <a href="https://www.gatsbyjs.com">Gatsby</a>*/}
+      {/*  </footer>*/}
+      {/*</div>*/}
     </>
   )
 }
